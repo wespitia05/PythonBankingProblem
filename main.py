60a950523f37011623ed11dc83ae720ecca3ef36
william <espiw@farmingdale.edu>
2025-06-20 16:07:21 -0400

    defining functions

---
+++ main.py
@@ -0,0 +1,16 @@
+# python banking problem
+
+# divide into separate sections:
+# show balance, withdrawal, deposit
+
+# this function will show us the balance
+def show_balance ():
+    pass
+
+# this function will handle the deposit methods
+def deposit ():
+    pass
+
+# this function will handle the withdrawal methods
+def withdrawal ():
+    pass
